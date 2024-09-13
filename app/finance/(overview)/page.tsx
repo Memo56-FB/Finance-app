2eb48fec398a88fcb5da40565fbd8a683c266676
Memo56-FB <memo56_fb@hotmail.com>
2024-09-13 14:53:11 -0600

    🎨 Feat(overview): responsive layout

--- app/finance/(overview)/page.tsx
+++ app/finance/(overview)/page.tsx
@@ -10,11 +10,15 @@ const page = () => {
     <>
       <h1 className='text-preset-1 text-grey-900'>Overview</h1>
       <Summary />
-      <section className='grid gap-200'>
-        <Pots />
-        <Transactions />
-        <Budgets />
-        <RecurringBills />
+      <section className='grid gap-200 xl:gap-0 xl:grid-cols-[auto_auto] xl:gap-x-300'>
+        <div className='flex flex-col gap-200 xl:gap-200'>
+          <Pots />
+          <Transactions />
+        </div>
+        <div className='flex flex-col gap-200 xl:gap-200'>
+          <Budgets />
+          <RecurringBills />
+        </div>
       </section>
     </>
   )
