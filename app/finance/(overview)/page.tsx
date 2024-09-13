15535160e535b6d1dae1856071dfccf68cb529fa
Memo56-FB <memo56_fb@hotmail.com>
2024-09-13 01:22:10 -0600

    🌚 Feat: add budgets card

--- app/finance/(overview)/page.tsx
+++ app/finance/(overview)/page.tsx
@@ -1,3 +1,4 @@
+import { Budgets } from '@/app/ui/overview/Budgets'
 import { Pots } from '@/app/ui/overview/Pots'
 import { Summary } from '@/app/ui/overview/Summary'
 import React from 'react'
@@ -7,8 +8,9 @@ const page = () => {
     <>
       <h1 className='text-preset-1 text-grey-900'>Overview</h1>
       <Summary />
-      <section>
+      <section className='grid gap-200'>
         <Pots />
+        <Budgets />
       </section>
     </>
   )
---
+++ app/ui/overview/Budgets.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BudgetsChart } from './BudgetsChart'
+
+export const Budgets = () => {
+  return (
+    <article>
+      <BudgetsChart />
+    </article>
+  )
+}
