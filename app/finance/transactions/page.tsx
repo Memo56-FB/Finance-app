3b85f58c73ed7aab0119e4ff6d057cbd89cf8db4
Memo56-FB <memo56_fb@hotmail.com>
2024-09-24 11:25:44 -0600

    🎨 Feat: add active styles on select option

--- app/finance/transactions/page.tsx
+++ app/finance/transactions/page.tsx
@@ -10,12 +10,16 @@ const page = () => {
     <>
       <h1 className='text-preset-1 text-grey-900'>Transactions</h1>
       <section className='bg-white mt-400 px-250 py-300 md:p-400 rounded-xl'>
-        <div className='grid grid-cols-[auto_auto_auto] gap-300 items-center'>
+        <div className='flex gap-300 items-center justify-between mb-300'>
           <InputSearch type='text' placeholder='Search transaction' />
-          <ResponsiveSelect options={['Latest', 'Profile', 'Billing', 'Team', 'Subscription']}>
-            <SortAscending weight='fill' size={20} className='text-grey-900' />
-          </ResponsiveSelect>
-          {/* <Funnel weight='fill' size={20} className='text-grey-900' /> */}
+          <div className='flex gap-300'>
+            <ResponsiveSelect label='Sort By' options={['Latest', 'Profile', 'Billing', 'Team', 'Subscription']}>
+              <SortAscending weight='fill' size={20} className='text-grey-900' />
+            </ResponsiveSelect>
+            <ResponsiveSelect label='Category' options={['All Transactions', 'Entertainment', 'Bills', 'Groceries', 'Dining Out', 'Transportation', 'Personal Care']}>   
+              <Funnel weight='fill' size={20} className='text-grey-900' />
+            </ResponsiveSelect>
+          </div>
         </div>
         <TransactionTableDesktop />
         <TransactionsTableMobile />
--- components/ui/ResponsiveSelect.tsx
+++ components/ui/ResponsiveSelect.tsx
@@ -4,62 +4,77 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSepara
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import React, { useState } from 'react'
 
-export const ResponsiveSelect = ({ options, children }: {
+export const ResponsiveSelect = ({ options, label, children }: {
   options: string[]
+  label: string
   children: React.ReactNode
 }) => {
-  const [selectedValue, setSelectedValue] = useState('latest')
+  const [selectedValue, setSelectedValue] = useState(options[0])
 
   return (
     <>
       <DropdownMenu>
         <DropdownMenuTrigger className='flex md:hidden'>
+          {/* ? An icon to show the dropdown menu */}
           {children}
         </DropdownMenuTrigger>
         <DropdownMenuContent className='px-250 py-150 rounded-lg *:p-0 text-preset-4 text-grey-900 grid gap-150'>
           {options.map((option, index) => {
-            if(options.length === index + 1){
+            const activeStyle = option === selectedValue ? 'text-preset-4-bold' : ''
+            if (options.length === index + 1) {
               return (
-                <DropdownMenuItem key={option}>
+                <DropdownMenuItem
+                  className={activeStyle}
+                  key={option}
+                  onClick={() => setSelectedValue(option)}
+                >
                   {option}
                 </DropdownMenuItem>
               )
             }
             return (
               <React.Fragment key={option}>
-                <DropdownMenuItem>{option}</DropdownMenuItem>
-                <DropdownMenuSeparator/>
+                <DropdownMenuItem
+                  onClick={() => setSelectedValue(option)}
+                  className={activeStyle}
+                >
+                  {option}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
               </React.Fragment>
             )
           })}
         </DropdownMenuContent>
       </DropdownMenu>
-      <Select value={selectedValue} onValueChange={(value) => setSelectedValue(value)}>
-        <SelectTrigger className="w-[180px] hidden md:flex">
-          <SelectValue>
-            {selectedValue}
-          </SelectValue>
-        </SelectTrigger>
-        <SelectContent className='px-250 py-150 rounded-lg text-preset-4 text-grey-900 [&>div]:grid [&>div]:p-0 [&>*]:gap-150'>
-          {options.map((option, index) => {
-            if(options.length === index + 1){
+      <div className='items-center gap-100 hidden md:flex'>
+        <label className='text-preset-4 text-grey-500 text-nowrap' htmlFor="">{label}</label>
+        <Select value={selectedValue} onValueChange={(value) => setSelectedValue(value)}>
+          <SelectTrigger>
+            <SelectValue>
+              {selectedValue}
+            </SelectValue>
+          </SelectTrigger>
+          <SelectContent className='px-250 py-150 rounded-lg text-preset-4 text-grey-900 [&>div]:grid [&>div]:p-0 [&>*]:gap-150'>
+            {options.map((option, index) => {
+              if (options.length === index + 1) {
+                return (
+                  <SelectItem key={option} value={option} className='p-0 focus:text-preset-4-bold' hideCheck={true}>
+                    {option}
+                  </SelectItem>
+                )
+              }
               return (
-                <SelectItem key={option} value={option} className='p-0 focus:text-preset-4-bold' hideCheck={true}>
-                  {option}
-                </SelectItem>
+                <React.Fragment key={option}>
+                  <SelectItem value={option} className='p-0 focus:text-preset-4-bold' hideCheck={true}>
+                    {option}
+                  </SelectItem>
+                  <DropdownMenuSeparator />
+                </React.Fragment>
               )
-            }
-            return (
-              <React.Fragment key={option}>
-                <SelectItem value={option} className='p-0 focus:text-preset-4-bold' hideCheck={true}>
-                  {option}
-                </SelectItem>
-                <DropdownMenuSeparator />
-              </React.Fragment>
-            )
-          })}
-        </SelectContent>
-      </Select>
+            })}
+          </SelectContent>
+        </Select>
+      </div>
     </>
   )
 }
