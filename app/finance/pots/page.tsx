2f27bed8c0457c7204d981a3ac6b2e86796d78d8
Memo56-FB <memo56_fb@hotmail.com>
2024-11-26 20:02:46 -0600

    ⭐ Feat: add responsive pots page and add props to potCard component

--- app/finance/pots/page.tsx
+++ app/finance/pots/page.tsx
@@ -9,7 +9,13 @@ const page = () => {
         <h1 className='text-preset-1 text-grey-900'>Pots</h1>
         <CreatePot />
       </header>
-      <PotCard />
+      <section className='grid gap-300 xl:grid-cols-2'>
+        <PotCard color='green' title='Savings' totalSaved={159.00} targetOf={2000} />
+        <PotCard color='navy' title='Concert Ticket' totalSaved={110.00} targetOf={150} />
+        <PotCard color='cyan' title='Gift' totalSaved={40.00} targetOf={60} />
+        <PotCard color='yellow' title='New Laptop' totalSaved={10.00} targetOf={1000} />
+        <PotCard color='purple' title='Holiday' totalSaved={531.00} targetOf={1440} />
+      </section>
     </>
   )
 }
--- app/ui/pots/PotCard.tsx
+++ app/ui/pots/PotCard.tsx
@@ -4,25 +4,30 @@ import { EditDeleteDropdown } from './EditDeleteDropdown'
 import { Progress } from '@/components/ui/Progress'
 import { Button } from '@/components/ui/button'
 
-export const PotCard = () => {
+export const  PotCard = ({title, totalSaved, targetOf, color}: {
+  title: string
+  totalSaved: number
+  targetOf: number
+  color: string
+}) => {
   return (
-    <article className='bg-white rounded-xl px-250 py-300 grid gap-400'>
+    <article className='bg-white rounded-xl px-250 py-300 md:p-300 grid gap-400'>
       <header className='flex justify-between'>
         <div className='flex gap-200 items-center'>
-          <Circle size={16} weight="fill" className='fill-green' />
-          <h1 className='text-preset-2 text-grey-900'>Savings</h1>
+          <Circle size={16} weight="fill" className={`fill-${color}`} />
+          <h1 className='text-preset-2 text-grey-900'>{title}</h1>
         </div>
         <EditDeleteDropdown />
       </header>
       <section className='grid gap-200'>
         <div className='flex justify-between items-center'>
           <p className='text-preset-4 text-grey-500'>Total Saved</p>
-          <p className='text-preset-1 text-grey-900'>$159.00</p>
+          <p className='text-preset-1 text-grey-900'>${totalSaved.toFixed(2)}</p>
         </div>
         <div className='grid justify-between items-center grid-cols-2 gap-150'>
-          <Progress color='green' max={100} value={7.95} className='rounded-md h-2 [&::-webkit-progress-bar]:p-0 col-span-2' />
-          <p className='text-preset-5-bold text-grey-500'>7.95%</p>
-          <p className='text-right text-preset-5 text-grey-500'>Target of $3,000</p>
+          <Progress color={color} max={targetOf} value={totalSaved} className='rounded-md h-2 [&::-webkit-progress-bar]:p-0 col-span-2' /> 
+          <p className='text-preset-5-bold text-grey-500'>{((totalSaved / targetOf) * 100).toFixed(2)}%</p>
+          <p className='text-right text-preset-5 text-grey-500'>Target of ${targetOf}</p>
         </div>
       </section>
       <div className='grid grid-cols-2 gap-200'>
